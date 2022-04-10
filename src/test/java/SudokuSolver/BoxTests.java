0bd1200640054c6e75a7d784f5ed320a8d464aba
Justin Moczynski <jmoczynski@ithaca.edu>
2022-04-10 07:56:48 -0400

    documented BoxTests class

---
+++ src/test/java/SudokuSolver/BoxTests.java
@@ -0,0 +1,37 @@
+package SudokuSolver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoxTests {
+
+    @Test
+    // isValidCellArray method tests
+    void isValidCellArrayTests(){
+        // TODO: implement tests
+        assertTrue(false);
+    }
+
+    @Test
+    // constructor, accessor, and mutator method tests
+    void constructorTests(){
+        // TODO: implement tests
+        assertTrue(false);
+    }
+
+    @Test
+    // getCell method tests
+    void getCellTests(){
+        // TODO: implement tests
+        assertTrue(false);
+    }
+
+    @Test
+    // checkComplete method tests
+    void checkCompleteTests(){
+        // TODO: implement tests
+        assertTrue(false);
+    }
+
+}
