69c2c60315c67e56add1b277e91c4a1f27d911fe
Justin Moczynski <jmoczynski@ithaca.edu>
2022-04-10 12:18:39 -0400

    began documentation of Puzzle class

--- src/main/java/SudokuSolver/Puzzle.java
+++ src/main/java/SudokuSolver/Puzzle.java
@@ -1,4 +1,44 @@
 package SudokuSolver;
 
 public class Puzzle {
+
+    private Box[] boxes;
+    private boolean isComplete;
+
+    /**
+     * Checks if Box array is valid:
+     * - exactly 9 Box objects
+     * - exactly 9 unique rows (1-9) with exactly 9 Cells
+     * - exactly 9 unique cols (1-9) with exactly 9 Cells
+     * @param arr desired Box array
+     * @return true if Box array is valid, false otherwise
+     */
+    public static boolean isValidBoxArray(Box[] arr){
+        // TODO: implement
+        return false;
+    }
+
+    public Puzzle(Box[] boxes){
+        // TODO: implement
+    }
+
+    public Box[] getBoxes(){
+        // TODO: implement
+        return null;
+    }
+
+    public Box getBox(int num){
+        // TODO: implement
+        return null;
+    }
+
+    public Cell getCell(int r, int c){
+        // TODO: implement
+        return null;
+    }
+
+    public void setCell(int r, int c, int v){
+        // TODO: implement
+    }
+
 }
